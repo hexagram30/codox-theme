19b455d1e4bb34273ddc1413879011a5d46f616a
Duncan McGreggor <oubiwann@gmail.com>
2019-01-16 08:41:56 -0500

    Updated for fork.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject clozhang/codox-theme "0.1.0-SNAPSHOT"
-  :description "A Codox Theme for Clozhang Projects Documentation"
-  :url "https://github.com/clozhang/codox-theme"
-  :license {:name "Eclipse Public License 1.0"
-            :url "https://www.eclipse.org/legal/epl-v10.html"
-            :year 2017
-            :key "epl-1.0"})
+(defproject hexagram30/codox-theme "0.1.0-SNAPSHOT"
+  :description "A Codox theme for hexagram30 projects' documentation"
+  :url "https://github.com/hexagram30/codox-theme"
+  :license {
+  	:name "Eclipse Public License 1.0"
+    :url "https://www.eclipse.org/legal/epl-v10.html"
+    :year 2017
+    :key "epl-1.0"})
